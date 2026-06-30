6b59c2ba082ba81213864467c87ed6910f932f8f
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-30 22:47:30 +0300

    fix: constructor accepting invalid length input

--- src/ship.js
+++ src/ship.js
@@ -2,6 +2,12 @@ export class Ship {
   #length;
 
   constructor(length) {
+    if (!Number.isInteger(length))
+      throw new TypeError("expected an integer for length");
+
+    if (length < 1 || length > 4)
+      throw new RangeError("length must be between 1 and 4");
+
     this.#length = length;
   }
 
