d5b2600e6fa9d08e49de5db6ac084e21b3dc39af
Andriy Sydorchuk <programmers130822@gmail.com>
2026-07-02 18:49:46 +0300

    test: add board setter tests

--- test/player.test.js
+++ test/player.test.js
@@ -29,5 +29,50 @@ describe("testing Player behaviour", () => {
       expect(r.board).toEqual(rB);
       expect(c.board).toEqual(cB);
     });
+
+    test("throws for non Gameboard or not null board", () => {
+      expect(() => new Player("real", [[], []])).toThrow(TypeError);
+      expect(() => new Player("computer", { board: true })).toThrow(TypeError);
+    });
+  });
+
+  describe("board setter", () => {
+    test("throws for non Gameboard or not null board", () => {
+      const p = new Player("real");
+
+      expect(() => (p.board = [[], []])).toThrow(TypeError);
+      expect(() => (p.board = { board: true })).toThrow(TypeError);
+    });
+
+    test("sets board", () => {
+      const p = new Player("real");
+      const pB = new Gameboard();
+
+      p.board = pB;
+
+      expect(p.board).not.toBeNull();
+      expect(p.board).toEqual(pB);
+    });
+
+    test("sets new board", () => {
+      const pB = new Gameboard();
+      const p = new Player("real", pB);
+
+      const newPB = new Gameboard();
+      p.board = newPB;
+
+      expect(p.board).not.toBe(pB);
+      expect(p.board).not.toBeNull();
+      expect(p.board).toBe(newPB);
+    });
+
+    test("resets board to null", () => {
+      const p = new Player("real", new Gameboard());
+
+      p.board = null;
+
+      expect(p.board).not.toEqual(new Gameboard());
+      expect(p.board).toBeNull();
+    });
   });
 });
