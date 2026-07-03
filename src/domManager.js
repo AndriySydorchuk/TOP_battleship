7682480e2f16a28577963d2758409bb875671c10
Andriy Sydorchuk <programmers130822@gmail.com>
2026-07-03 19:16:40 +0300

    feat: add board view content

--- src/domManager.js
+++ src/domManager.js
@@ -1,6 +1,8 @@
 import computerImage from "../assets/computer.png";
 import arrowleft from "../assets/arrowleft.svg";
 
+import { game } from "./game";
+
 const domManager = (() => {
   function renderViews() {
     renderStartView();
@@ -26,6 +28,21 @@ const domManager = (() => {
   }
 
   function renderBoardView() {
+    renderGoBackSection();
+
+    const boardViewEl = document.querySelector(".board-view");
+
+    const boardContentEl = document.createElement("div");
+    boardContentEl.classList.add("board-content");
+
+    boardViewEl.append(boardContentEl);
+
+    renderBoard();
+    renderShips();
+    renderActions();
+  }
+
+  function renderGoBackSection() {
     const boardViewEl = document.querySelector(".board-view");
 
     const topEl = document.createElement("div");
@@ -43,6 +60,66 @@ const domManager = (() => {
     topEl.append(backBtn);
 
     boardViewEl.append(topEl);
+  }
+
+  function renderBoard() {
+    const boardContentEl = document.querySelector(".board-content");
+
+    const boardEl = document.createElement("div");
+    boardEl.classList.add("board");
+
+    for (let i = 0; i < 10; i++) {
+      const lineEl = document.createElement("div");
+      lineEl.classList.add("board-line");
+
+      for (let j = 0; j < 10; j++) {
+        const cellEl = document.createElement("div");
+        cellEl.classList.add("board-cell");
+
+        lineEl.append(cellEl);
+      }
+
+      boardEl.append(lineEl);
+    }
+
+    boardContentEl.append(boardEl);
+  }
+
+  function renderShips() {
+    const boardContentEl = document.querySelector(".board-content");
+
+    const shipBoxEl = document.createElement("div");
+    shipBoxEl.classList.add("ship-box");
+
+    const boardCellEl = document.querySelector(".board-cell");
+
+    game.getShipsPool().forEach((ship) => {
+      const shipEl = document.createElement("div");
+      shipEl.classList.add("ship", `ship-${ship.length}`);
+
+      shipBoxEl.append(shipEl);
+    });
+
+    boardContentEl.append(shipBoxEl);
+  }
+
+  function renderActions() {
+    const boardContentEl = document.querySelector(".board-content");
+
+    const actionsBoxEl = document.createElement("div");
+    actionsBoxEl.classList.add("actions-box");
+
+    const shuffleBtn = document.createElement("button");
+    shuffleBtn.classList.add("shuffle-btn");
+    shuffleBtn.textContent = "Shuffle";
+
+    const playBtn = document.createElement("button");
+    playBtn.classList.add("play-btn");
+    playBtn.textContent = "Play";
+
+    actionsBoxEl.append(shuffleBtn, playBtn);
+
+    boardContentEl.append(actionsBoxEl);
   }
 
   function showBoardView() {
--- src/game.js
+++ src/game.js
@@ -1,13 +1,31 @@
 import { domManager } from "./domManager";
 import { eventManager } from "./eventManager";
+import { Ship } from "./ship";
 
 const game = (() => {
+  const shipsPool = [
+    new Ship(4),
+    new Ship(3),
+    new Ship(3),
+    new Ship(2),
+    new Ship(2),
+    new Ship(2),
+    new Ship(1),
+    new Ship(1),
+    new Ship(1),
+    new Ship(1),
+  ];
+
   function init() {
     domManager.renderViews();
     eventManager.handleViewSwitch();
   }
 
-  return { init };
+  function getShipsPool() {
+    return shipsPool;
+  }
+
+  return { init, getShipsPool };
 })();
 
 export { game };
