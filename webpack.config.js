bd26cad0e4c5bfff22cd9ec9bf0d111a9e7e740f
Andriy Sydorchuk <programmers130822@gmail.com>
2026-07-03 15:13:18 +0300

    feat: add rule for images

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
