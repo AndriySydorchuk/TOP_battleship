a1ad9faeec242646f9265b55e25bba09527af3ef
Andriy Sydorchuk <programmers130822@gmail.com>
2026-07-06 15:44:00 +0300

    feat: initialize player gameboards

--- src/game.js
+++ src/game.js
@@ -8,6 +8,18 @@ import { Gameboard } from "./gameboard";
 const game = (() => {
   let player1;
   let player2;
+  const placements = [
+    { ship: new Ship(4), coordinates: [1, 1], orientation: "v" },
+    { ship: new Ship(3), coordinates: [2, 4], orientation: "h" },
+    { ship: new Ship(3), coordinates: [7, 8], orientation: "v" },
+    { ship: new Ship(2), coordinates: [0, 4], orientation: "h" },
+    { ship: new Ship(2), coordinates: [8, 0], orientation: "h" },
+    { ship: new Ship(2), coordinates: [8, 6], orientation: "v" },
+    { ship: new Ship(1), coordinates: [4, 3], orientation: "h" },
+    { ship: new Ship(1), coordinates: [5, 6], orientation: "h" },
+    { ship: new Ship(1), coordinates: [5, 9], orientation: "h" },
+    { ship: new Ship(1), coordinates: [7, 3], orientation: "h" },
+  ];
 
   function init() {
     initPlayers();
@@ -20,7 +32,16 @@ const game = (() => {
 
   function initPlayers() {
     player1 = new Player(CONFIG.PLAYER_TYPE.REAL, new Gameboard());
+    initGameboard(player1, placements);
+
     player2 = new Player(CONFIG.PLAYER_TYPE.COMPUTER, new Gameboard());
+    initGameboard(player2, placements);
+  }
+
+  function initGameboard(player, placements) {
+    placements.forEach(({ ship, coordinates, orientation }) => {
+      player.board.placeShip(ship, coordinates, orientation);
+    });
   }
 
   return { init };
